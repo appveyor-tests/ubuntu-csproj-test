d426c4cefa4037a1e957ae6c51e1c60a29903189
Feodor Fitsner <feodor@appveyor.com>
2018-01-31 12:04:47 -0800

    "build" section added to yml

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember(Name = "firstName")]
+        public string FirstName { get; set; }
+
+        [DataMember(Name = "lastName")]
+        public string LastName { get; set; }
+
+        [DataMember(Name = "otherProps")]
+        public Hashtable OtherProps { get; set; }
+
+        [DataMember(Name = "age")]
+        public int? Age { get; set; }
+
+        public Person()
+        {
+            OtherProps = new Hashtable();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -8,7 +11,45 @@ namespace ConsoleApp1
         {
             int i = 0;
             Console.WriteLine("Hello World!");
+
+            var person = new Person { FirstName = "Feodor", LastName = "Fitsner" };
+            person.OtherProps["Key1"] = "Value1";
+            person.OtherProps["Key2"] = "Value2";
+
+            var json = ToJson(person);
+
+            var json1 = "{\"firstName\":\"Feodor\",\"lastName\":\"Fitsner\"}";
+
+            var p = FromJson<Person>(json);
+
             return 0; 
+        }
+
+        private static string ToJson<T>(T obj)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T), GetJsonSettings());
+
+            MemoryStream stream1 = new MemoryStream();
+            ser.WriteObject(stream1, obj);
+
+            return Encoding.UTF8.GetString(stream1.ToArray());
+        }
+
+        private static T FromJson<T>(string json)
+        {
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T), GetJsonSettings());
+            T deserializedUser = (T)ser.ReadObject(ms);
+            ms.Close();
+            return deserializedUser;
+        }
+
+        private static DataContractJsonSerializerSettings GetJsonSettings()
+        {
+            return new DataContractJsonSerializerSettings
+            {
+                UseSimpleDictionaryFormat = true
+            };
         }
     }
 }
